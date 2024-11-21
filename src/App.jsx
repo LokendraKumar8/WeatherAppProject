ef21371dd6dcf52e31a98038e6339a1292b70ca6
LokendraKumar8 <lokendrakumar582809@gmail.com>
2024-11-21 10:47:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import SearchBox from "./SearchBox";
+// import InfoBox from "./InfoBox";
+// import "./app.css";
+import WeatherApp from "./WeatherApp";
+function App() {
+    return (
+    <>
+      <WeatherApp/>
+    </>
+  );
+}
+
+export default App
